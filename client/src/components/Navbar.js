586786df08969231e76b138e9b3c3f47c88838f1
philliphdev <phannafo@us.ibm.com>
2018-07-26 07:47:23 -0400

    Updated Navbar className

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,29 +4,29 @@ import {Link} from 'react-router-dom'
 class Navbar extends Component {
     render() {
         return (
-                <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-              <a class="navbar-brand" href="#">Let's Ride</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+              <a className="navbar-brand" href="#">Let's Ride</a>
+              <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
               </button>
-              <div class="collapse navbar-collapse" id="navbarColor01">
-                <ul class="navbar-nav mr-auto">
-                  <li class="nav-item active">
-                    <Link class="nav-link" to="/" >Home <span class="sr-only">(current)</span></Link>
+              <div className="collapse navbar-collapse" id="navbarColor01">
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item active">
+                    <Link className="nav-link" to="/" >Home <span className="sr-only">(current)</span></Link>
                   </li>
-                  <li class="nav-item">
-                    <Link class="nav-link" to="/users">Users</Link>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/users">Users</Link>
                   </li>
-                  <li class="nav-item">
-                    <Link class="nav-link" to="/rides">Rides</Link>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/rides">Rides</Link>
                   </li>
-                  <li class="nav-item">
-                    <Link class="nav-link" to="/bikes">Bikes</Link>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/bikes">Bikes</Link>
                   </li>
                 </ul>
-                <form class="form-inline my-2 my-lg-0">
-                  <input class="form-control mr-sm-2" placeholder="Search" type="text" />
-                  <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                <form className="form-inline my-2 my-lg-0">
+                  <input className="form-control mr-sm-2" placeholder="Search" type="text" />
+                  <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
                 </form>
               </div>
             </nav>
