d8b4b763bfb3061e8d22fdf9bbbc270e2d1086a3
philliphdev <phannafo@us.ibm.com>
2018-07-27 13:37:11 -0400

    created show ride page

--- app/controllers/api/rides_controller.rb
+++ app/controllers/api/rides_controller.rb
@@ -5,14 +5,30 @@ class Api::RidesController < ApplicationController
   end
 
   def show
+    @ride = Ride.find(params[:id])
+    render json: @ride
   end
 
   def create
+    @ride = Ride.create!(ride_params)
+    render json: @ride
   end
 
   def update
+    @ride = Ride.find(params[:id])
+    @ride.update!(ride_params)
+    render json: @ride
   end
 
-  def delete
+  def destroy
+    @ride = Ride.find(params[:id]).delete
+    render status: :ok
   end
+
+  private
+
+  def ride_params
+      params.require(:ride).permit(:title, :ride_date, :description, :start_place, :end_place, :contact)
+  end
+
 end
