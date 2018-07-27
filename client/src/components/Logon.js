7872197ba2088c9967c525dea110738647629726
philliphdev <phannafo@us.ibm.com>
2018-07-26 22:07:48 -0400

    updated logon and register logic

--- client/src/components/Logon.js
+++ client/src/components/Logon.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
 class Logon extends Component {
+    
     render() {
         return (
             <div>
@@ -8,16 +10,15 @@ class Logon extends Component {
                     <div className="card-header">Logon</div>
                     <div className="card-body">
                         <div>
-                            <label className="col-form-label" for="email">email</label>
+                            <label className="col-form-label" value="email">email</label>
                             <input className="form-control" placeholder="email" id="email" type="text" />
                         </div>
                         <div>
-                            <label className="col-form-label" for="password">Password</label>
+                            <label className="col-form-label" value="password">Password</label>
                             <input className="form-control" placeholder="password" id="password" type="password" />
                         </div>
-                        <button type="button" className="btn btn-primary">GO!</button>
+                            <Link to="/users" className="btn btn-primary">GO!</Link>
                     </div>
-                    <button type="button" class="btn btn-outline-info">Sign UP</button>
                 </div>
             </div>
         );
--- client/src/components/Splashpage.js
+++ client/src/components/Splashpage.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react'
 import '../css/splashpage.css'
 import Logon from './Logon';
+import NewUser from './NewUser';
+import { Button } from '../../node_modules/@material-ui/core';
 
 class Splashpage extends Component {
+    state = {
+        user: {
+            name: '',
+            email: '',
+            password: '',
+            nickname: '',
+            comments: '',
+            photo_url: ''
+        },
+        isShowing: false
+    }
+    toggleIsShowing = () => {
+        this.setState({
+            isShowing: !this.state.isShowing
+        })
+    }
     render() {
         return (
             <div className="App">
                 <div>
-                <h1>Splash Page</h1>
-                <Logon />
+                    <h1>Splash Page</h1>
+                    {
+                        this.state.isShowing ?
+                            <NewUser
+                                newUser={this.newUser}
+                                handleChange={this.handleChange}
+                                user={this.state.user}
+                            />
+                            : <Logon />
+                            
+                    }
+                    <Button  onClick={this.toggleIsShowing}>
+                        {this.state.isShowing ? "Cancel" : "Register"}</Button>
                 </div>
             </div>
         );
